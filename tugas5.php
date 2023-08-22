d629fa621439a3c846e743f256a0552f6e89f84d
Caturputraramadani <caturputraramadani@smkwikrama.sch.id>
2023-08-22 12:19:30 +0700

    phpDasar modify

--- tugas5.php
+++ tugas5.php
@@ -1,25 +1,53 @@
-<?php
-// // Inisialisasi array untuk menyimpan nilai siswa
-// $nilai_siswa = array(10, 20, 3, 4, 5, 6, 7, 8, 9, 10);
+<!DOCTYPE html>
+<html>
 
-// // Menginput 10 nilai ujian siswa
-// for ($i = 1; $i <= 10; $i++) {
-//     echo "Masukkan nilai ujian siswa ke-$i: ";
-//     $nilai = floatval(readline());
-//     array_push($nilai_siswa, $nilai);
-// }
+<head>
+    <title>Nilai Tertinggi Siswa</title>
+</head>
 
-// // Mencari nilai tertinggi
-// $nilai_tertinggi = max($nilai_siswa);
+<body>
+    <h1>Input Nilai Ujian Siswa</h1>
 
-// // Menghitung berapa banyak siswa yang mendapatkan nilai tertinggi
-// $jumlah_tertinggi = 0;
-// foreach ($nilai_siswa as $nilai) {
-//     if ($nilai == $nilai_tertinggi) {
-//         $jumlah_tertinggi++;
-//     }
-// }
+    <form method="post">
+        <?php
+        // Inisialisasi array untuk menyimpan nilai ujian
+        $nilai_siswa = array();
 
-// // Menampilkan hasil
-// echo "Nilai tertinggi yang didapat siswa: $nilai_tertinggi\n";
-// echo "Jumlah siswa yang mendapatkan nilai tertinggi: $jumlah_tertinggi\n";
+        // Memproses form ketika tombol submit ditekan
+        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+            // Mengambil nilai-nilai ujian dari form
+            for ($i = 1; $i <= 10; $i++) {
+                $nilai = $_POST["nilai$i"];
+                $nilai_siswa[] = $nilai;
+            }
+
+            // Mencari nilai tertinggi
+            $nilai_tertinggi = max($nilai_siswa);
+
+            // Menghitung jumlah siswa yang mendapat nilai tertinggi
+            $jumlah_tertinggi = 0;
+            foreach ($nilai_siswa as $nilai) {
+                if ($nilai == $nilai_tertinggi) {
+                    $jumlah_tertinggi++;
+                }
+            }
+
+            // Menampilkan nilai tertinggi dan jumlah siswa yang mendapat nilai tertinggi
+            echo "<p>Nilai tertinggi: $nilai_tertinggi</p>";
+            echo "<p>Jumlah siswa yang mendapat nilai tertinggi: $jumlah_tertinggi</p>";
+        }
+        ?>
+
+        <!-- Form input nilai ujian -->
+        <p>Masukkan nilai ujian siswa:</p>
+        <?php
+        for ($i = 1; $i <= 10; $i++) {
+            echo "<input type='number' name='nilai$i' placeholder='Nilai siswa $i' required><br>";
+        }
+        ?>
+        <br>
+        <input type="submit" value="Hitung Nilai Tertinggi">
+    </form>
+</body>
+
+</html>
---
+++ tugas6.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Pencarian Juara Kelas</title>
+</head>
+
+<body>
+    <h1>Pencarian Juara Kelas</h1>
+    <form method="post" action="">
+        <?php
+        $totalSiswa = 15;
+        $siswaJuara = 0;
+
+        for ($i = 1; $i <= $totalSiswa; $i++) {
+            echo "<h2>Siswa $i</h2>";
+            echo "<label>Nama Siswa:</label>";
+            echo "<input type='text' name='namaSiswa[]'><br>";
+            echo "<label>Nilai MTK:</label>";
+            echo "<input type='number' name='nilaiMTK[]'><br>";
+            echo "<label>Nilai ING:</label>";
+            echo "<input type='number' name='nilaiING[]'><br>";
+            echo "<label>Nilai IND:</label>";
+            echo "<input type='number' name='nilaiIND[]'><br>";
+            echo "<label>Nilai DPK:</label>";
+            echo "<input type='number' name='nilaiDPK[]'><br>";
+            echo "<label>Nilai AGM:</label>";
+            echo "<input type='number' name='nilaiAGM[]'><br>";
+            echo "<label>Kehadiran:</label>";
+            echo "<input type='number' name='kehadiran[]'><br>";
+        }
+        ?>
+        <input type="submit" name="submit" value="Cari Juara">
+    </form>
+    <?php
+    if (isset($_POST['submit'])) {
+        $siswaJuara = 0;
+
+        for ($i = 0; $i < $totalSiswa; $i++) {
+            $totalNilai = $_POST['nilaiMTK'][$i] + $_POST['nilaiING'][$i] + $_POST['nilaiIND'][$i] + $_POST['nilaiDPK'][$i] + $_POST['nilaiAGM'][$i];
+            $kehadiran = $_POST['kehadiran'][$i];
+
+            if ($totalNilai >= 475 && $kehadiran == 100) {
+                $siswaJuara++;
+            }
+        }
+
+        echo "<h2>Jumlah Siswa Juara: $siswaJuara</h2>";
+    }
+    ?>
+</body>
+
+</html>
