cd05ddb4d7925e4dcbbda1b6bba9873401ccfdae
Caturputraramadani <caturputraramadani@smkwikrama.sch.id>
2023-08-23 15:10:15 +0700

    modify

--- tugas1.php
+++ tugas1.php
@@ -1,12 +1,33 @@
-<?php
-$A = 10;
-$B = 20;
-$C = 30;
+<!DOCTYPE html>
+<html lang="en">
 
-if ($A >= $B && $A >= $C) {
-    echo "$A";
-} elseif ($B >= $A && $B >= $C) {
-    echo "$B";
-} else {
-    echo "$C";
-}
+<body>
+    <form method="post">
+        <label>bil1</label>
+        <input type="number" name="bil1" required>
+        <label>bil2</label>
+        <input type="number" name="bil2" required>
+        <label>bil3</label>
+        <input type="number" name="bil3" required>
+        <button type="submit">input</button>
+    </form>
+    <?php
+    if ($_POST) {
+        $A = $_POST["bil1"];
+        $B = $_POST["bil2"];
+        $C = $_POST["bil3"];
+
+        if ($A > $B && $A > $C) {
+            echo "$A";
+        } elseif ($B > $A && $B > $C) {
+            echo "$B";
+        } else {
+            echo "$C";
+        }
+    }
+
+    ?>
+
+</body>
+
+</html>
